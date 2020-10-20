8a7e5465ad7758dfe8eefbc323f78cf85d4b800c
QiuMingZS <guanzhengcn@foxmail.com>
2020-10-20 08:18:39 +0800

    add new file bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    double vec[10];
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("Please input 10 numbers: %d\n", &vec[i]);
+    }
+    
+
+    return 0;
+}
